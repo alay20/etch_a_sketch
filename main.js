69538fcf62f26768a9b60eea5656d8b25488d7c4
Arthur Lay <arthur.lay@gmail.com>
2022-08-09 23:43:42 -0400

    Button added to change number of squares on each side of square container. New elements not fitting into squaure container properly yet.

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 var gridContainer = document.querySelector('.grid-container');
+gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 
-
-function makeGrid () {
-    for (let i = 0; i < 256; i++) {
+function makeGrid (area) {
+    for (let i = 0; i < area; i++) {
         var squareDiv = document.createElement('div');
         squareDiv.classList.add('square');
-        squareDiv.style.width = '33px'; 
-        squareDiv.style.height = '33px';
         squareDiv.style.borderStyle = 'solid';
         squareDiv.style.backgroundColor = 'white';
         squareDiv.style.borderWidth = '1px'
-        squareDiv.style.borderColor = 'black';
+        squareDiv.style.borderColor = 'grey';
         gridContainer.appendChild(squareDiv);
     }
 }
 
-function sketch() {
-    squareDiv.style.backgroundColor = 'black';
-}
-
-makeGrid();
+makeGrid(256);
 
 var squareDiv = document.querySelectorAll('.square');
 for (let i = 0; i < squareDiv.length; i++) {
@@ -28,6 +23,15 @@ for (let i = 0; i < squareDiv.length; i++) {
     })
 }
 
-// squareDiv.addEventListener('mouseenter', sketch);
-// squareDiv.addEventListener('mouseleave', sketch);
-// mouseenter 
+var btnClick = document.querySelector('.btn').addEventListener('click', function(){
+    var dimension = prompt('Select the number of squares on each side. Must be a whole number 0-100'); 
+    area = dimension*dimension
+    makeGrid(area);
+    gridContainer.style.gridTemplateColumns = 'repeat(dimension, 1fr)';
+    gridContainer.style.gridTemplateRows = 'repeat(dimension, 1fr)';
+})
+
+//how to input variable from prompt into repeat()??
+
+
+
