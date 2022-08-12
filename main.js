a0a4b17e77da686b158082ad3509961c02c2f0b8
Arthur Lay <arthur.lay@gmail.com>
2022-08-11 21:42:58 -0400

    The grid can now be changed to have 1 to 100 squares on each side by clicking the button and  entering an integer between 1 and 100 in the prompt window.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var gridContainer = document.querySelector('.grid-container');
 gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
-gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 function makeGrid (area) {
     for (let i = 0; i < area; i++) {
@@ -16,7 +15,7 @@ function makeGrid (area) {
 
 makeGrid(256);
 
-//To draw on grid in black
+//To sketch on grid in black
 
 var squareDiv = document.querySelectorAll('.square');
 for (let i = 0; i < squareDiv.length; i++) {
@@ -24,8 +23,10 @@ squareDiv[i].addEventListener('mouseenter', function() {
 this.style.backgroundColor = 'black';
 })}
 
+//Button to change grid dimensions
+
 var btnClick = document.querySelector('.btn').addEventListener('click', function(){
-    var dimension = prompt('Select the number of squares on each side. Must be a whole number 0-100'); 
+    var dimension = parseInt(prompt('Select the number of squares on each side. Must be a whole number 0-100')); 
     area = dimension*dimension
     //remove old grid
     var removeDiv = document.querySelectorAll('.square');
@@ -34,8 +35,7 @@ var btnClick = document.querySelector('.btn').addEventListener('click', function
     }
     //add new grid
     makeGrid(area);
-    gridContainer.style.gridTemplateColumns = 'repeat(30, 1fr)';
-    gridContainer.style.gridTemplateRows = 'repeat(30, 1fr)';
+    gridContainer.style.gridTemplateColumns = 'repeat('+dimension+', 1fr)';
     var squareDiv = document.querySelectorAll('.square');
         for (let i = 0; i < squareDiv.length; i++) {
         squareDiv[i].addEventListener('mouseenter', function() {
